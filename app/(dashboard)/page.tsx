23d1e56695fde34ff7cc0da1c4b3e8d1be83130e
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-02-16 15:36:52 +0330

    feat: Revamp Dashboard UI, Improve Routing & Update Dependencies
    - Updated orders and dashboard pages:
      - Standardized import statements (double quotes for consistency).
      - Revised text content to reflect new functionality.
      - Updated dashboard page with Radar report insights.
    
    - Enhanced layout structure:
      - Changed navigation items: "Dashboard" -> "Home", "Orders" -> "Map".
      - Updated icons to `HomeIcon` and `MapIcon` for improved UX.
      - Refactored `RootLayout` for cleaner structure.
    
    - Dependency updates:
      - Added `lucide-react` for icons.
      - Integrated `shadcn-ui` for UI improvements.
    
    - Updated `package.json` and `pnpm-lock.yaml` to reflect new dependencies.
    
    This commit enhances the app's clarity, navigation, and overall UI/UX.

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,14 @@
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import Link from 'next/link';
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Link from "next/link";
 
 export default function HomePage() {
-  
-
-  return (    
-      <Typography>
-        Welcome to <Link href="https://mui.com/toolpad/core/introduction">Toolpad Core!</Link>
-      </Typography>
+  return (
+    <Typography>
+      The Radar report has been extracted from the data of the DDoS detection
+      and Mitigation system, which has been deployed and operated by the TIC
+      company as the country's defense shield, providing effective protection
+      against attacks.
+    </Typography>
   );
 }
